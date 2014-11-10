ddedf466a53b314631c271941a308e063d35f7be
Joakim Rishaug <Joakimrishaug@gmail.com>
2014-11-10 23:48:49 +0100

    Added send question, added foundation for show single question

--- TestSiteFAQ/Scripts/App/FAQController.js
+++ TestSiteFAQ/Scripts/App/FAQController.js
@@ -1,15 +1,18 @@
-﻿var App = angular.module("App", []);
+﻿/// <reference path="../LIBJS/jquery-2.1.1.js" />
+var App = angular.module("App", []);
 
 App.controller("FAQController", function ($scope, $http) {
 
     var url = '/api/FAQ/';
 
-    $scope.sletteFeil = false;
-
     console.log("FAQ-controller.js found");
 
     function getAllFaqs() {
-        $scope.sletteFeil = false;
+        //define bools for single-page applcation
+        $scope.showAFAQ = false;
+        $scope.registrering = false;
+
+
         $http.get(url).
           success(function (allFaqs) {
               $scope.faqs = allFaqs;
@@ -19,9 +22,50 @@ App.controller("FAQController", function ($scope, $http) {
               console.log(status + data);
           });
     };
+    console.log("Much console");
     $scope.visFaqs = true;
     $scope.regKnapp = true;
     $scope.laster = true;
     getAllFaqs();
+
+
+    $scope.showSingleFAQ = function(FAQ) {
+        //define bools for single-page applcation
+        $scope.showAFAQ = true;
+        $scope.visFaqs = false;
+        $scope.registrering = false;
+
+        console.log("In ShowSingleFAQ");
+
+        $scope.currentHeading = FAQ.Heading;
+        $scope.currentDescription = FAQ.Description;
+
+        console.log($scope.currentHeading);
+        console.log($scope.currentDescription);
+    };
+
+    $scope.visSendSpm = function () {
+        $scope.epost = "";
+        $scope.sporsmal = "";
+        // for å unngå at noen av feltene gir "falske" feilmeldinger 
+        $scope.skjema.$setPristine();
+        $scope.regKnapp = false; // dette er knappen fra listen av kunder til reg-skjema
+        $scope.sendSpm = true;
+        $scope.visFaqs = false;
+        $scope.registrering = true; // dette er knappen for å registrere i form´en.
+
+        console.log("In showSendSpm");
+    };
+
+    $scope.sendQuestion = function () {
+        // lag et object for overføring til server via post
+        console.log("Inne i registerKunde");
+        var question = {
+            epost: $scope.epost,
+            sporsmal: $scope.sporsmal
+        };
+        console.log(question.epost);
+        console.log(question.sporsmal);
+    };
 
 });
